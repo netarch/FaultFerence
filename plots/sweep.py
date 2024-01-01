d2a161a1fc5e68a184aae7adb8604417eed7a183
Rahul Bothra <bothra2@illinois.edu>
2023-12-31 23:02:32 -0600

    update plotting script

--- plots/sweep.py
+++ plots/sweep.py
@@ -12,7 +12,7 @@ import matplotlib.font_manager as fm
 log_path = sys.argv[1]
 START_INDEX = 1
 ITERATIONS = 100
-TOPOLOGY_PREFIX = "rg"
+TOPOLOGY_PREFIX = "ft"
 DEGREE_SWITCH_MAPPING = {
     10: 125,
     12: 180,
@@ -38,7 +38,8 @@ SEQUENCE_SCHEME_MAPPING = {
 
 INFERENCE_SCHEME_MAPPING = {
     "Flock": " w/ Flock",
-    "Naive": " w/o Flock"
+    "Naive": " w/o Flock",
+    "Naive_old": " w/o Flock (old)"
 }
 
 ALL_STEPS = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
@@ -49,6 +50,10 @@ AVG_LAST_DEVICES = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
 PRECISION = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
 AVG_PRECISION = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+
+RECALL = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+AVG_RECALL = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+
 
 # Schema of the log path should be <logdir>/<topology>/<iteration>/<sequence_scheme>/<inference_scheme>/
 for topology in os.listdir(log_path):
@@ -61,18 +66,27 @@ for topology in os.listdir(log_path):
     for iter_index in range(START_INDEX, ITERATIONS + START_INDEX):
         iter_string = str(iter_index)
         iter_path = os.path.join(topology_path, iter_string)
+
+        # If some iterations are missing or still going on - this raises a warning
         if not iter_string in os.listdir(topology_path):
             print("WARNING:", topo_degree, "- does not have iteration", iter_string)
             continue
         for sequence_scheme in os.listdir(iter_path):
             sequence_path = os.path.join(iter_path, sequence_scheme)
+
+            # There are some configuration fails which are unique to each run kept in this directory
             if not os.path.isdir(sequence_path):
                 continue
+
             for inference_scheme in os.listdir(sequence_path):
                 inference_path = os.path.join(sequence_path, inference_scheme)
+
+                # If the run is still ongoing / failed due to some error - this raises a warning
                 if not "num_steps" in os.listdir(inference_path):
                     print("WARNING:", topo_degree, iter_string, inference_scheme, sequence_scheme, "- run does not have an output yet.")
                     continue
+
+                # We noticed that processes got killed sometimes due to memory constraints - here's a check for that.
                 logs = open(os.path.join(inference_path, "logs")).read()
                 if "Killed" in logs:
                     print("WARNING:", topo_degree, iter_string, inference_scheme, sequence_scheme, "- some iterations were killed in this run")
@@ -81,19 +95,26 @@ for topology in os.listdir(log_path):
                 last_devices = int(open(os.path.join(inference_path, "equivalent_devices")).readlines()[-2].split(", Devices:")[0].split("Size: ")[1])
                 last_devices_list = open(os.path.join(inference_path, "equivalent_devices")).readlines()[-2].split(", Devices: [")[1].split("]")[0].split(", ")
                 failed_device = int(open(os.path.join(iter_path, "initial.fails")).read().split(" ")[3])
-                if not last_devices_list == ['']:
-                    print(failed_device, last_devices_list)
+                
+                if last_devices_list == [""]:
+                    # HACK - Naive_Old had this bug where it reduced equivalent class to 0 because of an incorrect algorithm
+                    if inference_scheme == "Naive_old":
+                        last_devices = 2
+                        PRECISION[sequence_scheme][inference_scheme][topo_degree].append(0.5)
+                        RECALL[sequence_scheme][inference_scheme][topo_degree].append(1)
+                    else:
+                        print("WARNING:", topo_degree, iter_string, inference_scheme, sequence_scheme, "- number of equivalent devices reduced to 0. How?")
+                        PRECISION[sequence_scheme][inference_scheme][topo_degree].append(0)
+                        RECALL[sequence_scheme][inference_scheme][topo_degree].append(0)
+                else:
                     last_devices_list = [int(x) for x in last_devices_list]
                     if failed_device in last_devices_list:
                         PRECISION[sequence_scheme][inference_scheme][topo_degree].append(1/len(last_devices_list))
+                        RECALL[sequence_scheme][inference_scheme][topo_degree].append(1)
                     else:
                         PRECISION[sequence_scheme][inference_scheme][topo_degree].append(0)
-                if inference_scheme == "Naive" and last_devices == 0: # HACK
-                    last_devices = 2
-                    PRECISION[sequence_scheme][inference_scheme][topo_degree].append(0.5)
-                if last_devices == 0:
-                    print("WARNING:", topo_degree, iter_string, inference_scheme, sequence_scheme, "- number of equivalent devices reduced to 0. How?")
-                    continue
+                        RECALL[sequence_scheme][inference_scheme][topo_degree].append(0)
+
                 ALL_STEPS[sequence_scheme][inference_scheme][topo_degree].append(num_steps)
                 LAST_DEVICES[sequence_scheme][inference_scheme][topo_degree].append(last_devices)
 
@@ -102,10 +123,11 @@ for sequence_scheme in ALL_STEPS:
         for topo_degree in ALL_STEPS[sequence_scheme][inference_scheme]:
             all_steps = ALL_STEPS[sequence_scheme][inference_scheme][topo_degree]
             AVG_STEPS[sequence_scheme][inference_scheme][topo_degree] = np.mean(all_steps)
-            AVG_LAST_DEVICES[sequence_scheme][inference_scheme][topo_degree] = np.mean(LAST_DEVICES[sequence_scheme][inference_scheme][topo_degree])
             CONFIDENCE_INTERVAL[sequence_scheme][inference_scheme][topo_degree] = st.norm.interval(confidence=0.90, loc=np.mean(all_steps), scale=st.sem(all_steps))
-
+            
+            AVG_LAST_DEVICES[sequence_scheme][inference_scheme][topo_degree] = np.mean(LAST_DEVICES[sequence_scheme][inference_scheme][topo_degree])
             AVG_PRECISION[sequence_scheme][inference_scheme][topo_degree] = np.mean(PRECISION[sequence_scheme][inference_scheme][topo_degree])
+            AVG_RECALL[sequence_scheme][inference_scheme][topo_degree] = np.mean(RECALL[sequence_scheme][inference_scheme][topo_degree])
 
 
 fm.fontManager.addfont("./gillsans.ttf")
@@ -113,7 +135,7 @@ matplotlib.rcParams.update({'font.size': 24, 'font.family': "GillSans"})
 
 # colors = ['#cc6600', '#330066', '#af0505', '#db850d', '#a5669f', '#028413', '#000000', '#0e326d']
 # colors = ['seagreen', 'brown', 'skyblue', 'black']
-colors = ["#f1a200", "#995ec3", "#7fbf7b", "#cf4c32"]
+colors = ["#f1a200", "#995ec3", "#7fbf7b", "#cf4c32", "seagreen", "black"]
 markers = ['o', 's', '^', 'v', 'p', '*', 'p', 'h']
 # linestyles = ["-", ":", "-.", "dotted"]
 
@@ -167,19 +189,24 @@ legend = plt.legend(fontsize="22", markerscale=0.7, handlelength=0.7, handletext
 
 plt.savefig("steps-" + TOPOLOGY_PREFIX + ".png")
 
-print("ALL STEPS")
+print("** Summary of results **")
+THINGS_TO_PRINT = [AVG_PRECISION, AVG_RECALL]
+THINGS_TO_PRINT_TITLES = ["Average precision", "Average recall"]
 PRINTY = []
-THING_TO_PRINT = AVG_PRECISION
-for sequence_scheme in THING_TO_PRINT:
-    for inference_scheme in THING_TO_PRINT[sequence_scheme]:
-        for topo_degree in THING_TO_PRINT[sequence_scheme][inference_scheme]:
-            all_steps = THING_TO_PRINT[sequence_scheme][inference_scheme][topo_degree]
-            stringy = str(topo_degree) + " " + sequence_scheme + " " + inference_scheme + " " + str(all_steps)
-            PRINTY.append(stringy)
 
-PRINTY.sort()
-for line in PRINTY:
-    print(line)
+for index, THING_TO_PRINT in enumerate(THINGS_TO_PRINT):
+    PRINTY = []
+    for sequence_scheme in THING_TO_PRINT:
+        for inference_scheme in THING_TO_PRINT[sequence_scheme]:
+            for topo_degree in THING_TO_PRINT[sequence_scheme][inference_scheme]:
+                all_steps = THING_TO_PRINT[sequence_scheme][inference_scheme][topo_degree]
+                stringy = str(topo_degree) + " " + sequence_scheme + " " + inference_scheme + " " + str(all_steps)
+                PRINTY.append(stringy)
+    
+    print(THINGS_TO_PRINT_TITLES[index])
+    PRINTY.sort()
+    for line in PRINTY:
+        print(line)
 
 # print("******\n NUM DEVICES IN LAST STEP")
 # pprint(json.loads(json.dumps(LAST_DEVICES)))
